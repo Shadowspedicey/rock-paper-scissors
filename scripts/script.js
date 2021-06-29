7d7c6c2494b64c28c60d70bc67f08b5b7cdedc1a
Ahmed Khaled <shadowspedicey@gmail.com>
2021-06-29 09:48:38 +0200

    Added a way to check the response of the player

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,29 @@
-let computerSelection = function computerPlay()
+const computerSelection = function computerPlay()
 {
   const choices = ["Rock", "Paper", "Scissors"];
   let choice = choices[Math.floor(Math.random() * 3)];
   return choice;
 }
 
-let playerSelection = function PlayerPlay()
+const playerSelection = function()
 {
-  let response = prompt("Type your selection!");
+  let response = "";
+  response = prompt("Type your selection!").toString();
   response = response.toLowerCase();
   response = response.charAt(0).toUpperCase() + response.slice(1);
-  return response;
+
+  if (response === ("Rock") || response === ("Paper") || response === ("Scissors"))
+  {
+    return response;
+  }
+  else if (response == 0)
+  {
+    alert("Hey asshole, Type your fucking selection.");
+    return playerSelection();
+  }
+  else
+  {
+    alert("Something's not right! Choose between Rock, Paper or Scissors.");
+    return playerSelection();
+  }
 }
