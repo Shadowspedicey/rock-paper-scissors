8088f665ed8fe6e150e2a0c7a40b2c4491806e08
Ahmed Khaled <shadowspedicey@gmail.com>
2021-06-29 10:27:21 +0200

    created a round function and a game function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-const computerSelection = function computerPlay()
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+const computerSelection = function()
 {
   const choices = ["Rock", "Paper", "Scissors"];
   let choice = choices[Math.floor(Math.random() * 3)];
@@ -27,3 +31,108 @@ const playerSelection = function()
     return playerSelection();
   }
 }
+
+function playRound(_playerSelection, _computerSelection)
+{
+  _playerSelection = playerSelection();
+  _computerSelection = computerSelection();
+
+  if (_playerSelection === "Rock")
+  {
+    if (_computerSelection === _playerSelection)
+    {
+      console.log("Tie.");
+      console.log("Player score is: " + playerScore + ".");
+      console.log("Computer score is: " + computerScore + ".");
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Scissors")
+    {
+      playerScore++;
+      console.log("You win this round! Fuck you AI.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Paper")
+    {
+      computerScore++;
+      console.log("Fuck. You lost this one, champ.");
+      console.log("Player score is: " + playerScore + ".")
+      console.log("Computer score is: " + computerScore + ".")
+      console.log("Round number " + round + ".")
+    }
+  }
+  else if (_playerSelection === "Scissors")
+  {
+    if (_computerSelection === _playerSelection)
+    {
+      console.log("Tie.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Rock")
+    {
+      playerScore++;
+      console.log("You win this round! Fuck you AI.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Paper")
+    {
+      computerScore++;
+      console.log("Fuck. You lost this one, champ.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+  }
+  else if (_playerSelection === "Paper")
+  {
+    if (_computerSelection === _playerSelection)
+    {
+      console.log("Tie.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Rock")
+    {
+      playerScore++;
+      console.log("You win this round! Fuck you AI.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+    else if (_computerSelection === "Paper")
+    {
+      playerScore++;
+      console.log("Fuck. You lost this one, champ.");
+      console.log("Player score is: " + playerScore) + ".";
+      console.log("Computer score is: " + computerScore) + ".";
+      console.log("Round number " + round) + ".";
+    }
+  }
+  round++;
+}
+
+function game()
+{
+  for (let i = 0; i < 5; i++)
+  {
+    playRound();
+  }
+
+  if (playerScore > computerScore)
+  {
+    console.log("Player won!");
+  }
+  else
+  {
+    console.log("Computer won!");
+  }
+}
+
+game();
